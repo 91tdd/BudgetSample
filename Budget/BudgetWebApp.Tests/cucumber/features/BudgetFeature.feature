d7a9c16a8ab1951456b7b5131b70d320c519f806
hatelove <jay.hatelove@gmail.com>
2016-11-22 23:37:33 +0800

    add a scenario for adding budget and generated a default step definitions

---
+++ Budget/BudgetWebApp.Tests/cucumber/features/BudgetFeature.feature
@@ -0,0 +1,7 @@
+﻿Feature: BudgetFeature
+	
+
+
+Scenario: Add a budget for unique month	
+	When I add a buget 1000 for "2017-10"
+	Then there should be a existed record with budget 1000 for "2017-10"
---
+++ Budget/BudgetWebApp.Tests/cucumber/steps/BudgetFeatureSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace BudgetWebApp.Tests.cucumber.steps
+{
+    [Binding]
+    public class BudgetFeatureSteps
+    {
+        [When(@"I add a buget (.*) for ""(.*)""")]
+        public void WhenIAddABugetFor(int p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"there should be a existed record with budget (.*) for ""(.*)""")]
+        public void ThenThereShouldBeAExistedRecordWithBudgetFor(int p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
